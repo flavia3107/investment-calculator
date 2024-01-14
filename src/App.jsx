009221cf0d92a217f7ac40f1b97209fb041f8360
Flavia Kosti <flaviakosti@gmail.com>
2024-01-14 20:25:48 +0100

    Investment: Handle negative values

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import { useState } from 'react';
 import Results from './components/Results.jsx';
 
 function App() {
-    const [userInput, setUserInput] = useState({initialInvestment: 10000, annualInvestment: 1200, expectedReturn: 6, duration: 10})
+  const [userInput, setUserInput] = useState({initialInvestment: 10000, annualInvestment: 1200, expectedReturn: 6, duration: 10})
+  const inputIsValid = userInput.duration > 0;
 
-    function handleChange(field, newValue) {
+  function handleChange(field, newValue) {
         setUserInput(prevState => ({...prevState, [field]: +newValue}));
     }
   
@@ -14,7 +15,8 @@ function App() {
   <>
     <Header/>
       <UserInput onChange={handleChange} userInput={userInput} />
-      <Results userInput={userInput}/>
+      {!inputIsValid && <p className='center'>Please enter a duration greater than 0</p>}
+      {inputIsValid && <Results userInput={userInput}/>} 
   </>
   )
 }
